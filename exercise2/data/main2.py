1df3a6ce67b4fbb2389ec8ca7bb764bfdf449538
wmteske <wteske@iu.edu>
2024-10-29 15:45:09 -0500

    Update main2.py

--- exercise2/data/main2.py
+++ exercise2/data/main2.py
@@ -1,1 +1,11 @@
+import json
+from pathlib import Path
 
+data_dir = Path.cwd() / 'data'
+
+# Specify the path to your JSON file
+file_path = data_dir / 'sample_1.json'
+
+# Open and read the JSON file
+with open(file_path, 'r') as file:
+    data = json.load(file)  # Load JSON data as a Python dictionary
