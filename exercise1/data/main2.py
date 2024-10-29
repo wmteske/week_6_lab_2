96b999c6883e2a41d576b6dbd8dacce10f09dcb0
wmteske <wteske@iu.edu>
2024-10-29 15:32:54 -0500

    Create main2.py

---
+++ exercise1/data/main2.py
@@ -0,0 +1,19 @@
+from lxml import etree  # Import the etree module from the lxml library for XML parsing.
+from pathlib import Path  # Import Path from pathlib to handle file paths.
+
+# Define the directory containing the XML file by getting the current working directory and appending 'data' to it.
+data_dir = Path.cwd() / 'data'
+
+# Define the path to the XML file by appending the file name to the data directory.
+sample_xml_file = data_dir / 'sample_2.xml'
+
+# Parse the XML file and create an ElementTree object representing the XML structure.
+tree = etree.parse(sample_xml_file)
+
+"""
+Write the XPath expression below
+"""
+
+result = tree.xpath('')
+for item in result:
+    print(item)
